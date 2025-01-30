0b60ec053b097267f174bf461c61d8e453e46f29
Phatchara Champathong <phatchara.champathong@elev.ga.ntig.se>
2025-01-30 10:04:32 +0100

    first commit

---
+++ vinterproj25/People.cs
@@ -0,0 +1,22 @@
+using System.Security.Cryptography.X509Certificates;
+
+public class People
+{
+    public string Name;
+    public int Age;
+    
+    public int Height;
+    public int Weight;
+    public string DateAbirth;
+
+    public string Address;
+
+    public string Attribute;
+
+    
+    public  void ImposterCheck()
+    {
+        
+    }
+
+}
---
+++ vinterproj25/Program.cs
@@ -0,0 +1,7 @@
+﻿
+
+
+Console.WriteLine("Hi, you must be the new security checker. \nYour duty is to check if people that want to enter my party is actually invited.");
+Console.ReadLine();
+System.Console.WriteLine("Last time, some bastard just fake their name and the old security checker messed up. \nSo, I added more identity specification and I hope this time you won't disappoint me.");
+Console.ReadLine();
